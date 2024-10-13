5f257b72c4ff342c6b45f6a1153ab679eabc27a3
KsushaTarasova <ksutaras543@mail.ru>
2024-10-13 15:49:42 +0300

    Add annotation to class

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,7 +1,18 @@
 package org.example.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data //геттеры, сеттеры
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder //паттерн строитель
+@Entity
 public class User {
     private String username;
     private String firstname;
