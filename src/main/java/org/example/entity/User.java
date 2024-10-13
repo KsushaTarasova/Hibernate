31ec771d5607afc9f8e3f443b27824508b0d6b67
KsushaTarasova <ksutaras543@mail.ru>
2024-10-13 17:21:45 +0300

    Add Enumerated annotation

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,9 +1,6 @@
 package org.example.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,5 +22,6 @@ public class User {
     @Column(name = "birth_date")
     private LocalDate birthDate;
     private Integer age;
+    @Enumerated(EnumType.STRING)
     private Role role;
 }
