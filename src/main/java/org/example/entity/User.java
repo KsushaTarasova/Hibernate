7e535c9683e1052f29bf7cb46b8a11b7633b0c9e
KsushaTarasova <ksutaras543@mail.ru>
2024-10-13 15:47:20 +0300

    Create User class

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,11 @@
+package org.example.entity;
+
+import java.time.LocalDate;
+
+public class User {
+    private String username;
+    private String firstname;
+    private String lastname;
+    private LocalDate birthDate;
+    private Integer age;
+}
