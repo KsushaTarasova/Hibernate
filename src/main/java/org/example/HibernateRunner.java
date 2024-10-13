9d91de2efe70136fa2c3effea365e312dca8a283
KsushaTarasova <ksutaras543@mail.ru>
2024-10-13 16:07:53 +0300

    Add first user

--- src/main/java/org/example/HibernateRunner.java
+++ src/main/java/org/example/HibernateRunner.java
@@ -1,17 +1,31 @@
 package org.example;
 
 
+import org.example.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
         configuration.configure();
+        //configuration.addAnnotatedClass(User.class);
         try(SessionFactory sessionFactory = configuration.buildSessionFactory();
             Session session = sessionFactory.openSession();){
-            System.out.println("OK");
+            session.beginTransaction();
+
+            session.save(User.builder()
+                            .username("ksusha@mail.ru")
+                            .firstname("Kseniya")
+                            .lastname("Tarasova")
+                            .birthDate(LocalDate.of(2004, 2, 12))
+                            .age(19)
+                    .build());
+
+            session.getTransaction().commit();
         }
     }
 }
