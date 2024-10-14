46f76f7ce3a0cb3495b7e133008137030058789a
KsushaTarasova <ksutaras543@mail.ru>
2024-10-14 11:43:09 +0300

    Add env variables

--- src/main/java/org/example/HibernateRunner.java
+++ src/main/java/org/example/HibernateRunner.java
@@ -7,25 +7,32 @@ import org.example.entity.Role;
 import org.example.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
+        configuration
+                .setProperty(AvailableSettings.JAKARTA_JDBC_URL, System.getenv("DATABASE_URL"))
+                .setProperty(AvailableSettings.JAKARTA_JDBC_USER, System.getenv("DATABASE_USER"))
+                .setProperty(AvailableSettings.JAKARTA_JDBC_PASSWORD, System.getenv("DATABASE_PASSWORD"));
         configuration.configure();
         //configuration.addAnnotatedClass(User.class);
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
-             Session session = sessionFactory.openSession();) {
-            session.beginTransaction();
-            session.save(User.builder()
+             Session session = sessionFactory.openSession()) {
+
+            User user = User.builder()
                     .username("ksusha1@mail.ru")
                     .firstname("Kseniya")
                     .lastname("Tarasova")
                     .birthDate(new Birthday(LocalDate.of(2004, 2, 12)))
                     .role(Role.ADMIN)
-                    .build());
+                    .build();
 
+            session.beginTransaction();
+            session.save(user);
             session.getTransaction().commit();
         }
     }
